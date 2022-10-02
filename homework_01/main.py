5f1e5240615e19c803f13789376700ed747149cc
Nikolay Kolesnikov <nkolesnikov999@gmail.com>
2022-10-02 18:06:37 +0300

    Done Homework01

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,14 +3,33 @@
 Функции и структуры данных
 """
 
+def is_prime(n):
+    if n < 2:
+        return False
+    d = 2
+    while d * d <= n and n % d != 0:
+        d += 1
+    return d * d > n
 
-def power_numbers():
+def is_even(n):
+    return n % 2 == 0
+
+def is_odd(n):
+    return n % 2 != 0
+
+
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    sqNumbs = []
+    for num in numbers:
+        sqNumbs.append(num**2)
+
+    return sqNumbs
 
 
 # filter types
@@ -19,7 +38,7 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers, type_check):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +49,14 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    check_func = None
+    if type_check == ODD:
+        check_func = is_odd
+    if type_check == EVEN:
+        check_func = is_even
+    if  type_check == PRIME:
+        check_func = is_prime
+
+    return list(filter(check_func, numbers))
+
+
